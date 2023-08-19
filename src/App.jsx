136d280194fbf1cc6ff17d1c327766b5b346956c
david <palacingimenezdavid.fpllefia@gmail.com>
2023-08-19 13:01:32 +0200

    chore: follow mouse, activate and desactivate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {(
+      window.removeEventListener('pointermove', handleMove))
+      setPosition({x: 0, y: 0})
+    }
+  }, [enabled])
+
+  const handleChangeMouseFollower = () => {
+    setEnabled(!enabled)
+  }
+
   return (
-    <h1>Mouse follower in React</h1>
+    <main className="app">
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }} />
+      <h1>Mouse follower in React</h1>
+      <button onClick={handleChangeMouseFollower}>
+        {enabled ? 'Desactivar mouse follower' : 'Activar mouse follower'}
+      </button>
+    </main>
   )
 }
 
 export default App
+
